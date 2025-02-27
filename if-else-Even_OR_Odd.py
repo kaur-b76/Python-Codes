03844494b521e1f0667583ac534b77a7f40c9ffa
Bhavneet <91222684+kaur-b76@users.noreply.github.com>
2025-02-27 18:30:52 -0500

    Add files via upload
    This Python script determines whether a given number is even or odd.
    
    🚀 How It Works:
    The program asks the user to input a number.
    It prints the entered number.
    It checks if the number is divisible by 2 using the modulus (%) operator:
    If number % 2 == 0, the number is even.
    Otherwise, the number is odd.
    The result is then displayed to the user.

---
+++ if-else-Even_OR_Odd.py
@@ -0,0 +1,6 @@
+number_to_check = int(input("Please mention the number you want to check"))
+print(number_to_check)
+if number_to_check%2==0:
+    print("It is even number")
+else:
+    print("It is odd number")
